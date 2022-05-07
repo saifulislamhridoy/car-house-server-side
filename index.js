b60dbaeeff88b2b243a95c2d4667f2990ba4377f
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-05-08 02:34:43 +0600

    added post part

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app=express();
 const port = process.env.PORT || 5000
 
 // midleware
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.68qtz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+   await client.connect()
+   const productCollection =client.db('carHouse').collection('product')
+// POST
+   app.post('/product',async(req,res)=>{
+       const newProduct =req.body
+       const result = await productCollection.insertOne(newProduct)
+       res.send(result)
+   })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir)
 
 app.get('/',(req,res)=>{
     res.send('Running car house server')
