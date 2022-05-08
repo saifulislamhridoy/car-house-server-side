c5b36cf40318f22274c36af8b6b111e2e1ee89f4
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-05-08 19:37:43 +0600

    GET single product

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.68qtz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
@@ -28,6 +27,12 @@ async function run() {
             const cursor = productCollection.find(query)
             const products = await cursor.toArray()
             res.send(products)
+        })
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const product = await productCollection.findOne(query)
+            res.send(product)
         })
     }
     finally {
